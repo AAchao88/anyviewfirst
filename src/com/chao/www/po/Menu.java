1b3efb2617f591800d522889438e1f77f5796535
Wu Jinchao <WJC510175817@126.com>
2022-03-25 13:51:13 +0800

    feat(po):添加菜单类

---
+++ src/com/chao/www/po/Menu.java
@@ -0,0 +1,8 @@
+package com.chao.www.po;
+
+public class Menu {
+    public static void MenuOne(){
+        System.out.println("----------------------欢迎进入类语雀-----------------------");
+        System.out.println("-  ");
+    }
+}
